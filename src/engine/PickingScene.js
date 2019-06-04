a286635981aa73cf264e25ea72ec719b3a6afa61
Поздняков Алексей Николаевич <pozdnyakov_an@mgtniip.ru>
2019-06-04 09:14:49 +0300

    PickingScene

--- src/engine/PickingScene.js
+++ src/engine/PickingScene.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
 
-// This can be imported from anywhere and will still reference the same scene,
-// though there is a helper reference in Engine.pickingScene
+/**
+ * This can be imported from anywhere and will still reference the same scene,
+ * though there is a helper reference in Engine.pickingScene
+ */
 export default ( function () {
 
     var scene = new THREE.Scene();
