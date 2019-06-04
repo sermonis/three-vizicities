19749b26c9d3735ce88d2475224a3278862db88a
unknown <@WS-0099.mgtniip.ru>
2019-06-04 22:57:29 +0300

    PolylineLayer 39

--- src/layer/geometry/PolylineLayer.js
+++ src/layer/geometry/PolylineLayer.js
@@ -1,65 +1,87 @@
-// TODO: Move duplicated logic between geometry layrs into GeometryLayer
+/**
+ * TODO: Move duplicated logic between geometry layrs into GeometryLayer.
+ *
+ * TODO: Look at ways to drop unneeded references to array buffers, etc to
+ * reduce memory footprint.
+ *
+ * TODO: Provide alternative output using tubes and splines / curves.
+ *
+ * TODO: Support dynamic updating / hiding / animation of geometry
+ *
+ * This could be pretty hard as it's all packed away within BufferGeometry and
+ * may even be merged by another layer (eg. GeoJSONLayer).
+ *
+ * How much control should this layer support? Perhaps a different or custom
+ * layer would be better suited for animation, for example.
+ *
+ * TODO: Allow _setBufferAttributes to use a custom function passed in to
+ * generate a custom mesh.
+ */
 
-// TODO: Look at ways to drop unneeded references to array buffers, etc to
-// reduce memory footprint
+import * as THREE from 'three';
 
-// TODO: Provide alternative output using tubes and splines / curves
-
-// TODO: Support dynamic updating / hiding / animation of geometry
-//
-// This could be pretty hard as it's all packed away within BufferGeometry and
-// may even be merged by another layer (eg. GeoJSONLayer)
-//
-// How much control should this layer support? Perhaps a different or custom
-// layer would be better suited for animation, for example.
-
-// TODO: Allow _setBufferAttributes to use a custom function passed in to
-// generate a custom mesh
+import extend from 'lodash.assign';
 
 import Layer from '../Layer';
-import extend from 'lodash.assign';
-import * as THREE from 'three';
 import Geo from '../../geo/Geo';
-import {latLon as LatLon} from '../../geo/LatLon';
-import {point as Point} from '../../geo/Point';
+
+import { latLon as LatLon } from '../../geo/LatLon';
+import { point as Point } from '../../geo/Point';
+
 import PickingMaterial from '../../engine/PickingMaterial';
 import Buffer from '../../util/Buffer';
 
 class PolylineLayer extends Layer {
-  constructor(coordinates, options) {
-    var defaults = {
-      output: true,
-      interactive: false,
-      // Custom material override
-      //
-      // TODO: Should this be in the style object?
-      polylineMaterial: null,
-      onPolylineMesh: null,
-      onBufferAttributes: null,
-      // This default style is separate to Util.GeoJSON.defaultStyle
-      style: {
-        lineOpacity: 1,
-        lineTransparent: false,
-        lineColor: '#ffffff',
-        lineWidth: 1,
-        lineBlending: THREE.NormalBlending
-      }
-    };
 
-    var _options = extend({}, defaults, options);
+    /**
+     *
+     */
+    constructor( coordinates, options ) {
 
-    super(_options);
+        var defaults = {
 
-    // Return coordinates as array of lines so it's easy to support
-    // MultiLineString features (a single line would be a MultiLineString with a
-    // single line in the array)
-    this._coordinates = (PolylineLayer.isSingle(coordinates)) ? [coordinates] : coordinates;
+          output: true,
+          interactive: false,
 
-    // Polyline features are always flat (for now at least)
-    this._flat = true;
-  }
+          // Custom material override
+          //
+          // TODO: Should this be in the style object?
+          polylineMaterial: null,
+          onPolylineMesh: null,
+          onBufferAttributes: null,
 
-  _onAdd(world) {
+          // This default style is separate to Util.GeoJSON.defaultStyle
+          style: {
+
+            lineOpacity: 1,
+            lineTransparent: false,
+            lineColor: '#ffffff',
+            lineWidth: 1,
+            lineBlending: THREE.NormalBlending,
+
+          }
+
+        };
+
+        var _options = extend( {}, defaults, options );
+
+        super( _options );
+
+        // Return coordinates as array of lines so it's easy to support
+        // MultiLineString features (a single line would be a MultiLineString with a
+        // single line in the array)
+        this._coordinates = ( PolylineLayer.isSingle( coordinates ) ) ? [ coordinates ] : coordinates;
+
+        // Polyline features are always flat (for now at least)
+        this._flat = true;
+
+    }
+
+    /**
+     *
+     */
+    _onAdd( world ) {
+
     return new Promise((resolve, reject) => {
       this._setCoordinates();
 
@@ -113,41 +135,56 @@ class PolylineLayer extends Layer {
     });
   }
 
-  // Return center of polyline as a LatLon
-  //
-  // This is used for things like placing popups / UI elements on the layer
-  //
-  // TODO: Find proper center position instead of returning first coordinate
-  // SEE: https://github.com/Leaflet/Leaflet/blob/master/src/layer/vector/Polyline.js#L59
-  getCenter() {
+    // Return center of polyline as a LatLon
+    //
+    // This is used for things like placing popups / UI elements on the layer
+    //
+    // TODO: Find proper center position instead of returning first coordinate
+    // SEE: https://github.com/Leaflet/Leaflet/blob/master/src/layer/vector/Polyline.js#L59
+    /**
+     *
+     */
+    getCenter() {
     return this._center;
-  }
+    }
 
-  // Return line bounds in geographic coordinates
-  //
-  // TODO: Implement getBounds()
-  getBounds() {}
+    // Return line bounds in geographic coordinates
+    //
+    // TODO: Implement getBounds()
+    /**
+     *
+     */
+    getBounds() {}
 
-  // Get unique ID for picking interaction
-  _setPickingId() {
-    this._pickingId = this._options.pickingId = this.getPickingId();
-  }
+    // Get unique ID for picking interaction
+    /**
+     *
+     */
+    _setPickingId() {
+        this._pickingId = this._options.pickingId = this.getPickingId();
+    }
 
-  // Set up and re-emit interaction events
-  _addPickingEvents() {
-    // TODO: Find a way to properly remove this listener on destroy
-    this._world.on('pick-click-' + this._pickingId, (point2d, point3d, intersects) => {
-      // Re-emit click event from the layer
-      this.emit('click', this, point2d, point3d, intersects);
-    });
+    // Set up and re-emit interaction events
+    /**
+     *
+     */
+    _addPickingEvents() {
+        // TODO: Find a way to properly remove this listener on destroy
+        this._world.on('pick-click-' + this._pickingId, (point2d, point3d, intersects) => {
+        // Re-emit click event from the layer
+        this.emit('click', this, point2d, point3d, intersects);
+        });
 
-    this._world.on('pick-hover-' + this._pickingId, (point2d, point3d, intersects) => {
-      // Re-emit click event from the layer
-      this.emit('hover', this, point2d, point3d, intersects);
-    });
-  }
+        this._world.on('pick-hover-' + this._pickingId, (point2d, point3d, intersects) => {
+        // Re-emit click event from the layer
+        this.emit('hover', this, point2d, point3d, intersects);
+        });
+    }
 
-  static SetBufferAttributes(coordinates, options) {
+    /**
+     *
+     */
+    static SetBufferAttributes(coordinates, options) {
     return new Promise((resolve) => {
       var height = 0;
 
@@ -201,32 +238,44 @@ class PolylineLayer extends Layer {
         flat: flat
       });
     });
-  }
+    }
 
-  getBufferAttributes() {
-    return this._bufferAttributes;
-  }
+    /**
+     *
+     */
+    getBufferAttributes() {
+        return this._bufferAttributes;
+    }
 
-  // Used by external components to clear some memory when the attributes
-  // are no longer required to be stored in this layer
-  //
-  // For example, you would want to clear the attributes here after merging them
-  // using something like the GeoJSONLayer
-  clearBufferAttributes() {
-    this._bufferAttributes = null;
-  }
+    // Used by external components to clear some memory when the attributes
+    // are no longer required to be stored in this layer
+    //
+    // For example, you would want to clear the attributes here after merging them
+    // using something like the GeoJSONLayer
+    /**
+     *
+     */
+    clearBufferAttributes() {
+        this._bufferAttributes = null;
+    }
 
-  // Used by external components to clear some memory when the coordinates
-  // are no longer required to be stored in this layer
-  //
-  // For example, you would want to clear the coordinates here after this
-  // layer is merged in something like the GeoJSONLayer
-  clearCoordinates() {
-    this._coordinates = null;
-    this._projectedCoordinates = null;
-  }
+    // Used by external components to clear some memory when the coordinates
+    // are no longer required to be stored in this layer
+    //
+    // For example, you would want to clear the coordinates here after this
+    // layer is merged in something like the GeoJSONLayer
+    /**
+     *
+     */
+    clearCoordinates() {
+        this._coordinates = null;
+        this._projectedCoordinates = null;
+    }
 
-  static SetMesh(attributes, attributeLengths, flat, style, options) {
+    /**
+     *
+     */
+    static SetMesh(attributes, attributeLengths, flat, style, options) {
     var geometry = new THREE.BufferGeometry();
 
     for (var key in attributes) {
@@ -279,40 +328,50 @@ class PolylineLayer extends Layer {
       mesh: mesh,
       pickingMesh: pickingMesh
     });
-  }
 
-  // Convert and project coordinates
-  //
-  // TODO: Calculate bounds
-  _setCoordinates() {
-    this._bounds = [];
-    this._coordinates = this._convertCoordinates(this._coordinates);
+    }
 
-    this._projectedBounds = [];
-    this._projectedCoordinates = this._projectCoordinates();
+    // Convert and project coordinates
+    //
+    // TODO: Calculate bounds
+    /**
+     *
+     */
+    _setCoordinates() {
+        this._bounds = [];
+        this._coordinates = this._convertCoordinates(this._coordinates);
 
-    this._center = this._coordinates[0][0];
-  }
+        this._projectedBounds = [];
+        this._projectedCoordinates = this._projectCoordinates();
 
-  // Recursively convert input coordinates into LatLon objects
-  //
-  // Calculate geographic bounds at the same time
-  //
-  // TODO: Calculate geographic bounds
-  _convertCoordinates(coordinates) {
-    return coordinates.map(_coordinates => {
-      return _coordinates.map(coordinate => {
+        this._center = this._coordinates[0][0];
+    }
+
+    // Recursively convert input coordinates into LatLon objects
+    //
+    // Calculate geographic bounds at the same time
+    //
+    // TODO: Calculate geographic bounds
+    /**
+     *
+     */
+    _convertCoordinates(coordinates) {
+        return coordinates.map(_coordinates => {
+        return _coordinates.map(coordinate => {
         return LatLon(coordinate[1], coordinate[0]);
-      });
-    });
-  }
+        });
+        });
+    }
 
-  // Recursively project coordinates into world positions
-  //
-  // Calculate world bounds, offset and pointScale at the same time
-  //
-  // TODO: Calculate world bounds
-  _projectCoordinates() {
+    // Recursively project coordinates into world positions
+    //
+    // Calculate world bounds, offset and pointScale at the same time
+    //
+    // TODO: Calculate world bounds
+    /**
+     *
+     */
+    _projectCoordinates() {
     var point;
     return this._coordinates.map(_coordinates => {
       return _coordinates.map(latlon => {
@@ -330,9 +389,13 @@ class PolylineLayer extends Layer {
         return point;
       });
     });
-  }
 
-  static ToAttributes(line) {
+    }
+
+    /**
+     *
+     */
+    static ToAttributes(line) {
     // Three components per vertex
     var vertices = new Float32Array(line.verticesCount * 3);
     var colours = new Float32Array(line.verticesCount * 3);
@@ -385,38 +448,71 @@ class PolylineLayer extends Layer {
     }
 
     return attributes;
-  }
 
-  // Returns true if the line is flat (has no height)
-  isFlat() {
-    return this._flat;
-  }
-
-  // Returns true if coordinates refer to a single geometry
-  //
-  // For example, not coordinates for a MultiLineString GeoJSON feature
-  static isSingle(coordinates) {
-    return !Array.isArray(coordinates[0][0]);
-  }
-
-  destroy() {
-    if (this._pickingMesh) {
-      // TODO: Properly dispose of picking mesh
-      this._pickingMesh = null;
     }
 
-    this.clearCoordinates();
-    this.clearBufferAttributes();
+    // Returns true if the line is flat (has no height)
+    /**
+     *
+     */
+    isFlat() {
 
-    // Run common destruction logic from parent
-    super.destroy();
-  }
+        return this._flat;
+
+    }
+
+    // Returns true if coordinates refer to a single geometry
+    //
+    // For example, not coordinates for a MultiLineString GeoJSON feature
+    /**
+     *
+     */
+    static isSingle( coordinates ) {
+
+        return !Array.isArray( coordinates[ 0 ][ 0 ] );
+
+    }
+
+    /**
+     *
+     */
+    destroy() {
+
+        if ( this._pickingMesh ) {
+
+            // TODO: Properly dispose of picking mesh.
+            this._pickingMesh = null;
+
+        }
+
+        this.clearCoordinates();
+        this.clearBufferAttributes();
+
+        // Run common destruction logic from parent.
+        super.destroy();
+
+    }
+
+    /**
+     *  Proxy to destroy().
+     */
+    terminate() {
+
+        this.destroy();
+
+    }
+
 }
 
 export default PolylineLayer;
 
-var noNew = function(coordinates, options) {
-  return new PolylineLayer(coordinates, options);
+var noNew = function( coordinates, options ) {
+
+    return new PolylineLayer( coordinates, options );
+
 };
 
-export {noNew as polylineLayer};
+/**
+ *  Initialise without requiring new keyword.
+ */
+export { noNew as polylineLayer };
