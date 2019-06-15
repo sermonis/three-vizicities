7bfeb89cd9991d6579bc4c5b05a46cc03b1b3642
unknown <@WS-0099.mgtniip.ru>
2019-06-15 20:30:13 +0300

    util/color

--- src/util/Color.js
+++ src/util/Color.js
@@ -1,37 +1,39 @@
 /*
  * Color helpers
  */
-import * as THREE from 'three'
+import * as THREE from 'three';
 
-var Color = (function() {
+var Color = ( function () {
 
-    var random = function (color) {
+    var random = function ( color ) {
 
-        let $_color = color || 0xffffff
+        let _color = color || 0xffffff;
 
-        return new THREE.Color($_color * Math.random())
+        return new THREE.Color( _color * Math.random() );
 
-    }
+    };
 
     // TODO: Remove
     var material = function () {
 
-        return new THREE.MeshBasicMaterial({
-          color: 0xff0000,
-          // TODO: Remove this by implementing logic to make points clockwise
-          side: THREE.BackSide,
-          depthWrite: false,
-          transparent: true
-        })
+        return new THREE.MeshBasicMaterial( {
 
-    }
+            color: 0xff0000,
+            // TODO: Remove this by implementing logic to make points clockwise
+            side: THREE.BackSide,
+            depthWrite: false,
+            transparent: true,
+            
+        } );
+
+    };
 
     // TODO: Remove
-    var mesh = function (geometry, material) {
+    var mesh = function ( geometry, material ) {
 
-        return new THREE.Mesh(geometry, material)
+        return new THREE.Mesh( geometry, material )
 
-    }
+    };
 
     return {
 
@@ -39,8 +41,8 @@ var Color = (function() {
         material: material,
         mesh: mesh,
 
-    }
+    };
 
-})()
+})();
 
-export default Color
+export default Color;
